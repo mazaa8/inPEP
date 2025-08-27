09c3c4176ad024af3ee44a25f3bb85b162478ee3
mazaa8 <im.mazaa@gmail.com>
2025-08-27 19:10:00 +0300

    Update LandingPage.tsx

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -290,7 +290,7 @@ const LandingPage = () => {
                 textShadow: '0 1px 2px rgba(0,0,0,0.2)'
               }}
             >
-              © 2025 Indigo International • Comprehensive Healthcare Management • All Rights Reserved
+              © 2025 Indigo International • Patient Journey Management • All Rights Reserved
             </Typography>
           </Box>
         </Fade>
